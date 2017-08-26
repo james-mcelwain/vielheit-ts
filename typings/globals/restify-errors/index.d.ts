18b8eb31331ece57db407e8d9cd4fab7558d7297
James McElwain <jmcelwain@gmail.com>
2017-08-26 13:08:42 -0500

    rm prior to rebuilding toolchain

--- typings/globals/restify-errors/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'restify-errors' {
-    export class InternalServerError extends Error {
-    }
-    export class BadRequestError extends Error {
-    }
-}
--- typings/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="globals/pg-promise/index.d.ts" />
-/// <reference path="globals/restify-errors/index.d.ts" />
-/// <reference path="globals/react-toastr/index.d.ts" />
